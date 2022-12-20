005ee6d260510c9c7bdacb915ff55e253b86f5e3
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-12-20 11:05:57 +0100

    a

--- src/main/java/com/systementor/movies/model/MovieRepository.java
+++ src/main/java/com/systementor/movies/model/MovieRepository.java
@@ -2,5 +2,4 @@ package com.systementor.movies.model;
 
 import org.springframework.data.repository.CrudRepository;
 public interface MovieRepository extends CrudRepository<Movie, Integer> {
-    public Movie  findByEmail(String email) ;
 }
